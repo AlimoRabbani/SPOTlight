8ac31e37282ad58e4e89ecb2832fe7b1685df0be
Alimohammad Rabbani <amre2005@gmail.com>
2014-09-24 14:37:00 -0400

    using scheduler instead of while True for reading data.

--- Device.py
+++ Device.py
@@ -1,10 +1,8 @@
 __author__ = 'Alimohammad'
 import smbus
-import time
+import sched, time, math
 import threading
-import math
 import logging
-import os
 
 
 class RPi:
@@ -20,39 +18,39 @@ class RPi:
         self.bus = smbus.SMBus(1)
         self.occupancy_callback = occupancy_callback
 
-        temperature_thread = threading.Thread(target=self.read_temperature)
+        temperature_thread = threading.Thread(target=self.read_temperature_scheduler)
         temperature_thread.daemon = True
         temperature_thread.start()
 
-        motion_thread = threading.Thread(target=self.read_motion)
+        motion_thread = threading.Thread(target=self.read_motion_scheduler)
         motion_thread.daemon = True
         motion_thread.start()
 
-    def read_temperature(self):
-        while True:
-            data = self.bus.read_word_data(RPi.ADC_ADDRESS, RPi.TMP_CMD)
-            data = RPi.reverse_byte_order(data) & 0x0fff
-            temperature = (((data/4096.00)*5)-1.375)*1000/22.5
-            RPi.logger.info("[Temperature]" + str(temperature))
-            time.sleep(10)
+    def read_temperature_scheduler(self):
+        tmp_scheduler = sched.scheduler(time.time, time.sleep)
+        tmp_scheduler.enter(10, 1, self.read_temperature, (tmp_scheduler,))
+        tmp_scheduler.run()
 
-    def read_motion(self):
-        counter = 0
-        sum_of_squares = 0
-        sum_of_motion = 0
-        while True:
-            data = self.bus.read_word_data(RPi.ADC_ADDRESS, RPi.MOTION_CMD)
-            raw_motion = (RPi.reverse_byte_order(data) & 0x0fff) / 4.096
-            RPi.logger.info("[Motion]" + str(raw_motion))
-            sum_of_motion += raw_motion
-            sum_of_squares += pow(raw_motion, 2)
-            counter += 1
-            if counter == 240:
-                standard_deviation = math.sqrt((sum_of_squares / counter) - pow(sum_of_motion/counter, 2))
-                RPi.logger.info("[Occupancy]" + str(standard_deviation))
-                counter = sum_of_squares = sum_of_motion = 0
-                self.occupancy_callback(standard_deviation)
-            time.sleep(0.5)
+    def read_temperature(self, scheduler):
+        data = self.bus.read_word_data(RPi.ADC_ADDRESS, RPi.TMP_CMD)
+        data = RPi.reverse_byte_order(data) & 0x0fff
+        temperature = (((data/4096.00)*5)-1.375)*1000/22.5
+        RPi.logger.info("[Temperature]" + str(temperature))
+        scheduler.enter(10, 1, self.read_temperature, (scheduler,))
+
+    def read_motion_scheduler(self):
+        motion_scheduler = sched.scheduler(time.time, time.sleep)
+        motion_scheduler.enter(0.5, 1, self.read_motion, (motion_scheduler,))
+        motion_scheduler.run()
+
+    def read_motion(self, scheduler):
+        data = self.bus.read_word_data(RPi.ADC_ADDRESS, RPi.MOTION_CMD)
+        raw_motion = (RPi.reverse_byte_order(data) & 0x0fff) / 4.096
+        RPi.logger.info("[Motion]" + str(raw_motion))
+        # standard_deviation = math.sqrt((sum_of_squares / counter) - pow(sum_of_motion/counter, 2))
+        # RPi.logger.info("[Occupancy]" + str(standard_deviation))
+        # self.occupancy_callback(standard_deviation)
+        scheduler.enter(0.5, 1, self.read_motion, (scheduler,))
 
     @staticmethod
     def reverse_byte_order(data):
