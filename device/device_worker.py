18b92d06ef7503c5c5657c34279b09d7f0c70da8
Alimohammad Rabbani <amre2005@gmail.com>
2014-09-29 19:53:33 -0400

    RPC initiation complete!

--- device/device_worker.py
+++ device/device_worker.py
@@ -25,17 +25,19 @@ class DeviceService(rpyc.Service):
 def temperature_update_handler(temperature):
     decision_conn = rpyc.connect(Config.config["decision_service_address"], Config.config["decision_service_port"])
     decision_conn.root.temperature_updated(temperature)
+    decision_conn.close()
 
 
 def motion_update_handler(motion):
     decision_conn = rpyc.connect(Config.config["decision_service_address"], Config.config["decision_service_port"])
     decision_conn.root.motion_updated(motion)
+    decision_conn.close()
 
 
 if __name__ == "__main__":
     Config.initialize()
     server = ThreadedServer(DeviceService, hostname=Config.config["device_service_address"],
-                            port=Config.config["device_service_port"], logger=Config.logger, authenticator=None)
+                            port=Config.config["device_service_port"], logger=None, authenticator=None)
     RPi.start(temperature_update_handler, motion_update_handler)
     try:
         server.start()
