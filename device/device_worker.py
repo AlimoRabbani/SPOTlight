feed28e3d819c92694ca972817bb018e64576a7a
Alimohammad Rabbani <amre2005@gmail.com>
2014-09-30 11:00:50 -0400

    debugging info added for motion update handler

--- device/device_worker.py
+++ device/device_worker.py
@@ -29,6 +29,7 @@ def temperature_update_handler(temperature):
 
 
 def motion_update_handler(motion):
+    Config.logger.debug("motion update handler called")
     decision_conn = rpyc.connect(Config.config["decision_service_address"], Config.config["decision_service_port"])
     decision_conn.root.motion_updated(motion)
     decision_conn.close()
