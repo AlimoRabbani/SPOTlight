ee9794284e477038bec89faf5ed65aecd2741674
Alimohammad Rabbani <amre2005@gmail.com>
2014-10-30 18:47:21 -0400

    removing data_files

--- device/setup.py
+++ device/setup.py
@@ -5,6 +5,5 @@ from distutils.core import setup
 
 setup(name="device",
       version="1.0",
-      scripts=["device_worker.py", "config.py", "spotlight_devices.py", "device_updater.py"],
-      data_files=("", ["config_rpi.json", "config_service.json", "config_update.json"])
+      scripts=["device_worker.py", "config.py", "spotlight_devices.py", "device_updater.py", "config_rpi.json", "config_service.json", "config_update.json"],
       )
