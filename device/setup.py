1e383bbf09d3e3d715b3e9f18ba42aa82a69743e
Alimohammad Rabbani <amre2005@gmail.com>
2014-10-30 18:16:48 -0400

    fix for data_files

--- device/setup.py
+++ device/setup.py
@@ -6,5 +6,5 @@ from distutils.core import setup
 setup(name="device",
       version="1.0",
       scripts=["device_worker.py", "config.py", "spotlight_devices.py", "device_updater.py"],
-      data_files=["config_rpi.json", "config_service.json", "config_update.json"]
+      data_files=("", ["config_rpi.json", "config_service.json", "config_update.json"])
       )
