f6f5cfa9ad5b2467c5c764c60e51fb08b8271fb2
Alimohammad Rabbani <amre2005@gmail.com>
2014-10-31 11:08:17 -0400

    version updated

--- device/setup.py
+++ device/setup.py
@@ -4,7 +4,7 @@ from esky import bdist_esky
 from distutils.core import setup
 
 setup(name="device",
-      version="1.2",
+      version="1.3",
       scripts=["device_worker.py", "config.py", "spotlight_devices.py", "device_updater.py", ],
       data_files=[("", ["config_rpi.json", "config_service.json", "config_update.json"])]
       )
