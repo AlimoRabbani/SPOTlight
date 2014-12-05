1a8e410a9d959926b30372f210535583a65c988e
Alimohammad Rabbani <amre2005@gmail.com>
2014-12-05 18:12:07 -0500

    reverse proxy stuff added

--- web/views/common_views.py
+++ web/views/common_views.py
@@ -19,8 +19,8 @@ def index():
         if user is not None:
             if user.authenticate(form.password.data):
                 if login_user(user, remember=form.remember.data):
-                    current_app.logger.debug(url_for("user_views.devices_view", _external=True))
-                    return redirect(request.args.get("next") or url_for("user_views.devices_view", _external=True))
+                    current_app.logger.debug(url_for("user_views.devices_view"))
+                    return redirect(request.args.get("next") or url_for("user_views.devices_view"))
             else:
                 error = "Email and password do not match!"
         else:
@@ -36,7 +36,7 @@ def about():
         if user is not None:
             if user.authenticate(form.password.data):
                 if login_user(user, remember=form.remember.data):
-                    return redirect(request.args.get("next") or url_for("user_views.devices_view", _external=True))
+                    return redirect(request.args.get("next") or url_for("user_views.devices_view"))
             else:
                 error = "Email and password do not match!"
         else:
@@ -47,4 +47,4 @@ def about():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for("common_views.index", _external=True))
+    return redirect(url_for("common_views.index"))
