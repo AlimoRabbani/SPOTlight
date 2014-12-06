85273f1dc14d3a4e1d70e8fa65bc42a3fcc53886
Alimohammad Rabbani <amre2005@gmail.com>
2014-12-05 19:00:01 -0500

    trying to fix!

--- web/main.py
+++ web/main.py
@@ -9,30 +9,10 @@ from views.common_views import common_views
 from views.user_views import user_views
 from views.admin_views import admin_views
 from data_model import User
-
-class ReverseProxied(object):
-    def __init__(self, app):
-        self.app = app
-
-    def __call__(self, environ, start_response):
-        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
-        if script_name:
-            environ['SCRIPT_NAME'] = script_name
-            path_info = environ['PATH_INFO']
-            if path_info.startswith(script_name):
-                environ['PATH_INFO'] = path_info[len(script_name):]
-
-        scheme = environ.get('HTTP_X_Forwarded_Proto', '')
-        if scheme:
-            environ['wsgi.url_scheme'] = scheme
-        server = environ.get('HTTP_X_FORWARDED_SERVER', '')
-        if server:
-            environ['HTTP_HOST'] = server
-        return self.app(environ, start_response)
-
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ReverseProxied(app.wsgi_app)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app.register_blueprint(common_views)
 app.register_blueprint(user_views)
 app.register_blueprint(admin_views)
