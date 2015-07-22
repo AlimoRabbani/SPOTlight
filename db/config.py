06c2eb3fca62d2ec6428c3279a1ce3f0c1b61eef
Alimohammad Rabbani <amre2005@gmail.com>
2015-07-22 15:29:40 -0400

    increased db log backup count

--- db/config.py
+++ db/config.py
@@ -24,7 +24,7 @@ class Config:
         logger.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
-        file_handler = logging.handlers.RotatingFileHandler('db.log', maxBytes=20000000, backupCount=5)
+        file_handler = logging.handlers.RotatingFileHandler('db.log', maxBytes=200000000, backupCount=100)
         file_handler.setLevel(logging.INFO)
 
         console_handler = logging.StreamHandler()
